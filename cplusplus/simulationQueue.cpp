7f60b7d5828389d29a9ea51ed5ea0b4c959f91a6
Pablo_Linux <pleyva2004@gmail.com>
2025-10-15 15:04:58 -0400

    Starting C++ Scripts

---
+++ cplusplus/simulationQueue.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+
+int main(){
+	std::cout << "Simulating Queuing System" << std::endl;
+	
+	int t; // time
+	int Na; // number of arrivals
+	int n; // number of customers in the system
+	 
+	
+	return 0;
+}
